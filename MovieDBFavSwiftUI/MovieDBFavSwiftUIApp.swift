4c2fffd96e877a3978cff8b7488ae0bc26192931
Reinaldo Camargo <camargo.reinaldo@hotmail.com>
2024-05-03 11:56:57 -0300

    Initial Commit

---
+++ MovieDBFavSwiftUI/MovieDBFavSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDBFavSwiftUIApp.swift
+//  MovieDBFavSwiftUI
+//
+//  Created by Reinaldo Camargo on 03/05/24.
+//
+
+import SwiftUI
+
+@main
+struct MovieDBFavSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
